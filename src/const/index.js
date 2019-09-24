89d638a1135848b164b1807b24f33f82c0720877
rexbean <rexbean36@gmail.com>
2019-09-23 17:55:09 -0700

    Update index.js

--- src/const/index.js
+++ src/const/index.js
@@ -8,5 +8,3 @@ export const radius = 100;
 
 export const BACKEND_URL =
   "https://api.betterdoctor.com/2016-03-01/doctors?location=";
-export const API_KEY = "1d1a8e6fd0ddd4861a92b546b26fa419";
-export const GOOGLE_API_KEY = "AIzaSyBg44cLGi8gotWDO-VXvikZ5x_eGUF_XlM";
